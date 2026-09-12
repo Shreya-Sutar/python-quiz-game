c099a21102a04327809926f65f0425bc5921aae3
Shreya Sutar <shreyasutar@173@gmail.com>
2026-09-12 08:41:27 +0530

    Add JSON error handling

--- src/main.py
+++ src/main.py
@@ -3,8 +3,17 @@ import json
 from quiz import Quiz
 
 
-with open("data/questions.json", "r") as file:
-    questions = json.load(file)
+try:
+    with open("data/questions.json", "r") as file:
+        questions = json.load(file)
+
+except FileNotFoundError:
+    print("Error: Questions file was not found.")
+    exit()
+
+except json.JSONDecodeError:
+    print("Error: Questions file contains invalid JSON.")
+    exit()
 
 
 quiz = Quiz(questions)
